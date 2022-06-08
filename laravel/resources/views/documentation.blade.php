cd481ebb5a67a796fc57580e12df91879fb0f196
JustAPC <andrea.pontillo@libero.it>
2022-06-08 13:00:10 +0200

    modifiche

--- laravel/resources/views/documentation.blade.php
+++ laravel/resources/views/documentation.blade.php
@@ -8,26 +8,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Primi passi su Laravel</title>
 </head>
-
-<style>
-    ul li {
-        list-style-type: none;
-        display: inline;
-    }
-
-    ul a {
-        text-decoration: none;
-        color: white;
-        margin-right: 15px;
-    }
-
-    ul a:hover {
-        color: #423b64
-    }
-
-</style>
 
 <body style="background-image: url(https://images6.alphacoders.com/676/thumb-1920-676642.png); background-size:cover">
     <header class=" w-50 d-flex justify-content-between mx-auto py-5">
